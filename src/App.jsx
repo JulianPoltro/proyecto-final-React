b55f5074c7d9378d65ac6178cbfdf435b5a5b315
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-12-05 20:59:17 -0300

    Creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home"
-import About from "./About"
+import Cotizador from "./Cotizador"
 import Historial from "./Historial"
 import NotFound from "./NotFound"
 const App = () => {
@@ -8,8 +7,7 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/nosotros" element={<About />}></Route>
+          <Route path="/" element={<Cotizador />}></Route>
           <Route path="/historial" element={<Historial />}></Route>
           <Route path="*" element={<NotFound />}></Route>
 
---
+++ src/Cotizador.jsx
@@ -0,0 +1,3 @@
+const Cotizador = () => <h2>Cotizador</h2>
+
+export default Cotizador
