1c6c98c174ac37a97c509783fe2541f44581001a
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-12-05 23:58:28 -0300

    stringify

--- src/hook/useStorage.jsx
+++ src/hook/useStorage.jsx
@@ -4,11 +4,11 @@ const useStorage = (clave, valorInicial) => {
   const [valor, setValor] = useState(() => {
     let storage = localStorage.getItem(clave);
     if (storage) return JSON.parse(storage);
-    localStorage.setItem(clave, valorInicial);
+    localStorage.setItem(clave, JSON.stringify(valorInicial));
     return valorInicial;
   });
 
-  useEffect(() => localStorage.setItem(clave, valor), [valor]);
+  useEffect(() => localStorage.setItem(clave, JSON.stringify(valor)), [valor]);
 
   return [valor, setValor];
 };
