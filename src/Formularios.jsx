ca97c9c2e8f41634dbae18f0b2b9c4f5fb3d1326
Julian Poltronieri <JulianPoltronieri@hotmail.com>
2023-12-06 22:55:12 -0300

    json y fetch

--- src/Formularios.jsx
+++ src/Formularios.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import useStorage from "./hook/useStorage";
 
 const Formulario = () => {
-  const [vivienda, tipoVivienda] = useStorage("vivienda", []);
+  const [opcionEdificio, setOpcionEdificio] = useState([]);
+  const [opcionConstruccion, setOpcionConstruccion] = useState([]);
+
+  const datos = () => {
+    fetch("/data.json")
+      .then((respuesta) => {
+        if (!respuesta.ok) {
+          throw new Error("Error al cargar los datos");
+        }
+        return respuesta.json();
+      })
+      .then((data) => {
+        setOpcionEdificio(data.TipoEdificacion);
+        setOpcionConstruccion(data.TipoConstruccion);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  console.log(opcionEdificio, opcionConstruccion);
+
 
   return (
     <>
@@ -11,8 +33,9 @@ const Formulario = () => {
             Seleccionar el tipo de edificación
           </label>
           <select name="Edificio" id="TipoEdificio">
-          <option value={0} disabled defaultValue={0}>Selecciona un tipo de edificación</option>
-          
+            <option value={0} disabled defaultValue={0}>
+              Selecciona un tipo de edificación
+            </option>
           </select>
         </fieldset>
         <fieldset>
@@ -20,14 +43,23 @@ const Formulario = () => {
             Seleccionar el tipo de construcción
           </label>
           <select name="Construccion" id="TipoConstruccion">
-            <option value={0} disabled defaultValue={0}>Selecciona un tipo de construcción</option>
+            <option value={0} disabled defaultValue={0}>
+              Selecciona un tipo de construcción
+            </option>
           </select>
         </fieldset>
         <fieldset>
           <label htmlFor="metrosCuadrados">
             Ingresar la cantidad de metros cuadrados
           </label>
-          <input type="number" name="metrosCuadrados" id="metrosCuadrados" min={10} max={2000} defaultValue={10}/>
+          <input
+            type="number"
+            name="metrosCuadrados"
+            id="metrosCuadrados"
+            min={10}
+            max={2000}
+            defaultValue={10}
+          />
         </fieldset>
       </form>
     </>
